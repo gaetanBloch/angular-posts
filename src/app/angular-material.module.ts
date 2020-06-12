ef1b96cf02e6a42961c29faddadcb0ae1b372c90
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-12 15:14:43 +0200

    Styling the Post Input with Material Design

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   imports: [
-    MatInputModule
+    MatInputModule,
+    MatCardModule
   ],
   exports: [
-    MatInputModule
+    MatInputModule,
+    MatCardModule
   ]
 })
 export class AngularMaterialModule {
