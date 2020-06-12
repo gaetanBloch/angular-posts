d55961e7d67a8f9c2f675d3c36b267ad46781d5c
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-12 23:59:10 +0200

    Unsubscribe to posts Subscription in Post List Component

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,14 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Post } from '../post.model';
 import { PostService } from '../post.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
+  private postsSubscription = new Subscription();
   posts: Post[] = [];
 
   constructor(private postService: PostService) {
@@ -16,8 +18,16 @@ export class PostListComponent implements OnInit {
 
   ngOnInit(): void {
     this.posts = this.postService.getPosts();
-    this.postService.getPostUpdateListener().subscribe(posts => {
-      this.posts = posts;
-    });
+    this.postsSubscription = this.postService.getPostUpdateListener()
+      .subscribe(posts => {
+          this.posts = posts;
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    if (this.postsSubscription) {
+      this.postsSubscription.unsubscribe();
+    }
   }
 }
