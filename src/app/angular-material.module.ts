5cb277647607eb9714fd0dec07adb633df13b241
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-12 15:07:34 +0200

    Added Angular Material Module with a first Component Module

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  imports: [
+    MatInputModule
+  ],
+  exports: [
+    MatInputModule
+  ]
+})
+export class AngularMaterialModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
