3fac5d6c575fab9cae92d4d2f4080cc843d873c9
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-12 14:14:58 +0200

    Listening to click event of the button

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  onAddPost = () => {
+    console.log('Clicked!');
+  };
 }
