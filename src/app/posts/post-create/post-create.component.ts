3dea73718076f9d6fce8360886ef48302314a646
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-13 09:33:49 +0200

    Added Validation

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PostService } from '../post.service';
 
 @Component({
@@ -17,8 +17,8 @@ export class PostCreateComponent implements OnInit {
   ngOnInit(): void {
     this.form = this.fb.group(
       {
-        title: [''],
-        content: ['']
+        title: ['', [Validators.required, Validators.minLength(3)]],
+        content: ['', Validators.required]
       }
     );
   }
