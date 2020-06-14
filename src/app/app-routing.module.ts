6f404b4ecb1c3c57b665beaa8d1cc06165fcfe00
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-14 17:13:38 +0200

    Added signup Link to the Header and the route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'create', component: PostCreateComponent },
-  { path: 'edit/:postId', component: PostCreateComponent }
+  { path: 'edit/:postId', component: PostCreateComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
