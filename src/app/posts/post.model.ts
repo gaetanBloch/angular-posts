4d1b9159c5358662a6db33fde7541518fb05ecfd
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-15 09:30:01 +0200

    Check if the user logged in is the same that created the post to be able to delete or edit the post

--- src/app/posts/post.model.ts
+++ src/app/posts/post.model.ts
@@ -1,7 +1,10 @@
+import { User } from '../auth/user.model';
+
 export interface Post {
   _id?: string;
   title: string;
   content: string;
   imageUrl?: string;
   image?: string;
+  creator?: User;
 }
