197cb0afe379dc2cd0d2fd209f20ca0aafa05022
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-15 06:43:21 +0200

    Check the authentication status in the header to display the right controls

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 import { AuthService } from '../auth/auth.service';
 
 @Component({
@@ -6,12 +8,22 @@ import { AuthService } from '../auth/auth.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  isLogin = false;
+export class HeaderComponent implements OnInit, OnDestroy {
+  private authStatusSubSubscription = new Subscription();
+  isAuthenticated = false;
 
   constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authStatusSubSubscription = this.authService.getAuthStatusListener()
+      .subscribe(isAuth => {
+        this.isAuthenticated = isAuth;
+      });
   }
 
+  ngOnDestroy() {
+    if (this.authStatusSubSubscription) {
+      this.authStatusSubSubscription.unsubscribe();
+    }
+  }
 }
